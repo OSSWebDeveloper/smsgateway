bd963e721ba380d3ec64e9e40e1ba45d54df6a1f
MR_Coder <105047495+OSSWebDeveloper@users.noreply.github.com>
2026-06-27 11:29:55 +0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,9 +4,16 @@ call workon musoyevschool
 
 cd /d C:\Users\user\Desktop\IT-Center-Shofirkon\
 
-
+REM Django serverni ishga tushirish
 start cmd /k "py manage.py runserver"
 
-timeout /t 3 >nul
+REM Ngrokni ishga tushirish
+start cmd /k "ngrok http --url=roughy-outgoing-iguana.ngrok-free.app 8080"
 
+REM Server ochilishi uchun kutish
+timeout /t 5 >nul
+
+REM Chrome’da saytni ochish
 start chrome http://127.0.0.1:8000/
+
+exit
